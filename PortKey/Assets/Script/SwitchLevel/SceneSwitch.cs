e17feee6fd972d67bad1269e179f6f531e272643
DBPalmer <dbpalmer@usc.edu>
2024-07-04 13:44:55 -0700

    Prompt users with a tutorial before the FIRST time seeing a level

--- PortKey/Assets/Script/SwitchLevel/SceneSwitch.cs
+++ PortKey/Assets/Script/SwitchLevel/SceneSwitch.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using PortKey.Assets.Script;
 
 //code from https://www.youtube.com/watch?v=PpIkrff7bKU
 
@@ -9,104 +10,96 @@ public class SceneSwitch : MonoBehaviour
 {
     int MAP_SCENE_BUILD_NUMBER = 7;
 
+    //LEVELS
     public void loaderLvl1()
     {
-
-        GameLevelsManager.Instance.Level = 1; //setting the current level globally that player is on
-
-        SceneManager.LoadScene("Level1");
+        if (!TutorialInfo.lvl1) {
+            loaderTut();
+            TutorialInfo.lvl1 = true;
+        } else
+        {
+            GameLevelsManager.Instance.Level = 1; //setting the current level globally that player is on
+            SceneManager.LoadScene("Level1");
+        }
     }
-
     public void loaderLvl2()
     {
         GameLevelsManager.Instance.Level = 2; //setting the current level globally that player is on
-
-
         SceneManager.LoadScene("Level2");
     }
-
     public void loaderLvl3()
     {
-
-        GameLevelsManager.Instance.Level = 3; //setting the current level globally that player is on
-
-
-        SceneManager.LoadScene("Level3");
+        if (!TutorialInfo.lvl3)
+        {
+            loaderTut2();
+            TutorialInfo.lvl3 = true;
+        } else
+        {
+            GameLevelsManager.Instance.Level = 3; //setting the current level globally that player is on
+            SceneManager.LoadScene("Level3");
+        }
     }
-
     public void loaderLvl4()
     {
-
-        GameLevelsManager.Instance.Level = 4; //setting the current level globally that player is on
-
-
-        SceneManager.LoadScene("Level4");
+        if (!TutorialInfo.lvl4)
+        {
+            loaderTut3();
+            TutorialInfo.lvl4 = true;
+        }
+        else
+        {
+            GameLevelsManager.Instance.Level = 4; //setting the current level globally that player is on
+            SceneManager.LoadScene("Level4");
+        }
     }
-
     public void loaderLvl5()
     {
-
-        GameLevelsManager.Instance.Level = 5; //setting the current level globally that player is on
-
-
-        SceneManager.LoadScene("Level5");
+        if (!TutorialInfo.lvl5)
+        {
+            loaderTut3();
+            TutorialInfo.lvl5 = true;
+        }
+        else
+        {
+            GameLevelsManager.Instance.Level = 5; //setting the current level globally that player is on
+            SceneManager.LoadScene("Level5");
+        }
     }
 
+    //TUTORIALS
     public void loaderTut()
     {
-
-        //GameLevelsManager.Instance.Level = 4; //setting the current level globally that player is on
-
-
         SceneManager.LoadScene("Tutorial");
     }
-
     public void loaderTut2()
     {
-
-        //GameLevelsManager.Instance.Level = 4; //setting the current level globally that player is on
-
-
         SceneManager.LoadScene("Tutorial2");
     }
-
     public void loaderTut3()
     {
-
-        //GameLevelsManager.Instance.Level = 4; //setting the current level globally that player is on
-
-
         SceneManager.LoadScene("Tutorial3");
     }
-
     public void loaderTut4()
     {
-
-        //GameLevelsManager.Instance.Level = 4; //setting the current level globally that player is on
-
-
         SceneManager.LoadScene("Tutorial4");
     }
 
+    //MENUS
     public void loaderMenu()
     {
         SceneManager.LoadScene("Menu");
     }
-
     public void loaderMenuTut()
     {
         SceneManager.LoadScene("TutorialMenu");
     }
-
     public void loaderStarter()
     {
         SceneManager.LoadScene("Starter");
     }
-
     public void loadMap()
     {
         SceneManager.LoadScene("Map");
-        //SceneManager.LoadScene(MAP_SCENE_BUILD_NUMBER);
     }
 
 }
---
+++ PortKey/Assets/Script/TutorialInfo.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+public static class TutorialInfo
+{
+    public static bool lvl1 = false;
+    public static bool lvl3 = false;
+    public static bool lvl4 = false;
+    public static bool lvl5 = false;
+}
