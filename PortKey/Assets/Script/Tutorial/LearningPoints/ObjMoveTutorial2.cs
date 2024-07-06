612d9457f080bd93996bc8b6642506a235b94e60
Wardah Jabeen <jabeen@usc.edu>
2024-07-06 12:52:18 -0700

    blinking the object

--- PortKey/Assets/Script/Tutorial/LearningPoints/ObjMoveTutorial2.cs
+++ PortKey/Assets/Script/Tutorial/LearningPoints/ObjMoveTutorial2.cs
@@ -9,11 +9,14 @@ public class ObjMoveTutorial2 : MonoBehaviour
     public float speed = 2f;
 
     GameControllerTutorial2 gameController;
+    Vector3 originalScale;
+    bool isBlinking = false;
 
     // Start is called before the first frame update
     void Start()
     {
         gameController = FindObjectOfType<GameControllerTutorial2>();
+        originalScale = transform.localScale;
     }
 
     // Update is called once per frame
@@ -22,15 +25,39 @@ public class ObjMoveTutorial2 : MonoBehaviour
         if (gameController.canMove)
         {
             transform.Translate(Vector3.down * speed * Time.deltaTime);
+            isBlinking = false;
         }
         if (transform.position.y <= -3f && transform.gameObject.tag != "Obstacle")
         {
             gameController.canMove = false;
+            if (!isBlinking)
+            {
+                isBlinking = true;
+                StartCoroutine(BlinkObject());
+            }
         }
     }
 
     void OnDestroy()
     {
         gameController.canMove = true;
+
+        // Stop blinking stuff
+        StopCoroutine(BlinkObject());
+        transform.localScale = originalScale;
+        isBlinking = false;
+    }
+
+    IEnumerator BlinkObject()
+    {
+        Debug.Log("uhh hello?");
+        while (isBlinking)
+        {
+            transform.localScale = Vector3.zero;
+            yield return new WaitForSeconds(0.5f);
+
+            transform.localScale = originalScale;
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
--- PortKey/Assets/Script/Tutorial/ObjMoveTutorial.cs
+++ PortKey/Assets/Script/Tutorial/ObjMoveTutorial.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class ObjMoveTutorial : MonoBehaviour
 {
     public float moveSpeed = 2f;
-
     public float speed = 2f;
 
     GameControllerTutorial gameController;
+    Vector3 originalScale;
+    bool isBlinking = false;
 
     // Start is called before the first frame update
     void Start()
     {
         gameController = FindObjectOfType<GameControllerTutorial>();
+        originalScale = transform.localScale;
     }
 
     // Update is called once per frame
@@ -22,15 +24,40 @@ public class ObjMoveTutorial : MonoBehaviour
         if (gameController.canMove)
         {
             transform.Translate(Vector3.down * speed * Time.deltaTime);
+            isBlinking = false;
         }
         if (transform.position.y <= -3f && transform.gameObject.tag != "Obstacle")
         {
             gameController.canMove = false;
+
+            if (!isBlinking)
+            {
+                isBlinking = true;
+                StartCoroutine(BlinkObject());
+            }
         }
     }
 
     void OnDestroy()
     {
         gameController.canMove = true;
+
+        // Stop blinking stuff
+        StopCoroutine(BlinkObject());
+        transform.localScale = originalScale;
+        isBlinking = false;
+    }
+
+    IEnumerator BlinkObject()
+    {
+        Debug.Log("uhh hello?");
+        while (isBlinking)
+        {  
+            transform.localScale = Vector3.zero;
+            yield return new WaitForSeconds(0.5f); 
+
+            transform.localScale = originalScale;
+            yield return new WaitForSeconds(0.5f); 
+        }
     }
 }
