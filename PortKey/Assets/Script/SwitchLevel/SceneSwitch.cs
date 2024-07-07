af2da1bf19a1c6e6227fd060bb3e898f0994fd96
DBPalmer <dbpalmer@usc.edu>
2024-07-07 15:05:52 -0700

    back button for the prop menu

--- PortKey/Assets/Script/SwitchLevel/SceneSwitch.cs
+++ PortKey/Assets/Script/SwitchLevel/SceneSwitch.cs
@@ -17,12 +17,14 @@ public class SceneSwitch : MonoBehaviour
             loaderTut();
         } else
         {
+            TutorialInfo.lastScene = 1;
             GameLevelsManager.Instance.Level = 1; //setting the current level globally that player is on
             SceneManager.LoadScene("Level1");
         }
     }
     public void loaderLvl2()
     {
+        TutorialInfo.lastScene = 2;
         GameLevelsManager.Instance.Level = 2; //setting the current level globally that player is on
         SceneManager.LoadScene("Level2");
     }
@@ -33,6 +35,7 @@ public class SceneSwitch : MonoBehaviour
             loaderTut2();
         } else
         {
+            TutorialInfo.lastScene = 3;
             GameLevelsManager.Instance.Level = 3; //setting the current level globally that player is on
             SceneManager.LoadScene("Level3");
         }
@@ -45,6 +48,7 @@ public class SceneSwitch : MonoBehaviour
         }
         else
         {
+            TutorialInfo.lastScene = 4;
             GameLevelsManager.Instance.Level = 4; //setting the current level globally that player is on
             SceneManager.LoadScene("Level4");
         }
@@ -57,6 +61,7 @@ public class SceneSwitch : MonoBehaviour
         }
         else
         {
+            TutorialInfo.lastScene = 5;
             GameLevelsManager.Instance.Level = 5; //setting the current level globally that player is on
             SceneManager.LoadScene("Level5");
         }
@@ -65,21 +70,25 @@ public class SceneSwitch : MonoBehaviour
     //TUTORIALS
     public void loaderTut()
     {
+        TutorialInfo.lastScene = -1;
         SceneManager.LoadScene("Tutorial");
         TutorialInfo.lvl1 = true;
     }
     public void loaderTut2()
     {
+        TutorialInfo.lastScene = -2;
         SceneManager.LoadScene("Tutorial2");
         TutorialInfo.lvl3 = true;
     }
     public void loaderTut3()
     {
+        TutorialInfo.lastScene = -3;
         SceneManager.LoadScene("Tutorial3");
         TutorialInfo.lvl4 = true;
     }
     public void loaderTut4()
     {
+        TutorialInfo.lastScene = -4;
         SceneManager.LoadScene("Tutorial4");
         TutorialInfo.lvl5 = true;
     }
@@ -87,19 +96,61 @@ public class SceneSwitch : MonoBehaviour
     //MENUS
     public void loaderMenu()
     {
+        TutorialInfo.lastScene = 100;
         SceneManager.LoadScene("Menu");
     }
     public void loaderMenuTut()
     {
+        TutorialInfo.lastScene = 100;
         SceneManager.LoadScene("TutorialMenu");
     }
     public void loaderStarter()
     {
+        TutorialInfo.lastScene = 100;
         SceneManager.LoadScene("Starter");
     }
     public void loadMap()
     {
         SceneManager.LoadScene("Map");
+    }
+
+    //BACK BUTTON
+    public void BackButton()
+    {
+        switch (TutorialInfo.lastScene)
+        {
+            case 1:
+                loaderLvl1();
+                break;
+            case 2:
+                loaderLvl2();
+                break;
+            case 3:
+                loaderLvl3();
+                break;
+            case 4:
+                loaderLvl4();
+                break;
+            case 5:
+                loaderLvl5();
+                break;
+            case -1:
+                loaderTut();
+                break;
+            case -2:
+                loaderTut2();
+                break;
+            case -3:
+                loaderTut3();
+                break;
+            case -4:
+                loaderTut4();
+                break;
+            case 100:
+            default:
+                loaderMenu();
+                break;
+        }
     }
 
 }
--- PortKey/Assets/Script/TutorialInfo.cs
+++ PortKey/Assets/Script/TutorialInfo.cs
@@ -6,4 +6,5 @@ public static class TutorialInfo
     public static bool lvl3 = false;
     public static bool lvl4 = false;
     public static bool lvl5 = false;
+    public static int lastScene = 100;
 }
