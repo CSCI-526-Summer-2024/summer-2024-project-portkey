a6f9df33e71324fd7bbb57ecbe0077b9ce4b7a70
Wardah Jabeen <jabeen@usc.edu>
2024-07-15 18:04:03 -0700

    added new db link for analytics

--- PortKey/Assets/Script/Tutorial/GameControllerTutorial.cs
+++ PortKey/Assets/Script/Tutorial/GameControllerTutorial.cs
@@ -606,9 +606,9 @@ public class GameControllerTutorial : MonoBehaviour
 
         if (ConstName.SEND_ANALYTICS == true)
         {
-            RestClient.Post("https://portkey-2a1ae-default-rtdb.firebaseio.com/metric1_analytics.json", playerData);
-            RestClient.Post("https://portkey-2a1ae-default-rtdb.firebaseio.com/beta_playtesting_analytics.json", playerData);
-            RestClient.Post("https://portkey-2a1ae-default-rtdb.firebaseio.com/playtesting1_analytics.json", playerData);
+            RestClient.Post("https://portkey-2a1ae-default-rtdb.firebaseio.com/gold_podtesting_analytics.json", playerData);
+     //       RestClient.Post("https://portkey-2a1ae-default-rtdb.firebaseio.com/beta_playtesting_analytics.json", playerData);
+      //      RestClient.Post("https://portkey-2a1ae-default-rtdb.firebaseio.com/playtesting1_analytics.json", playerData);
         }
     }
 
